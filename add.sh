bae1c1f253c3e9c58b151f8220caed58732dd8b4
wernom <loic.sagona@gmail.com>
2018-02-19 12:03:16 +0100

    Correction typo dans add.sh rm.sh done

--- add.sh
+++ add.sh
@@ -1,20 +1,17 @@
 #!/bin/dash
 
 if test ! -f "$1";then
-    echo "Error : file $1 does not exist"
-<<<<<<< HEAD
+    echo "Error : file '$(basename "$1")' does not exist"
     exit 1
 fi
-=======
-fi
+
 
 
 mkdir $(dirname $1)/.version
 cp $1 $(dirname $1)/.version/$(basename $1).1
-cp $1 $(dirname $1)/.version/$(basename $1).last
+cp $1 $(dirname $1)/.version/$(basename $1).latest
 echo "Added a new file under versioning: '$1'"
 
 
 
 
->>>>>>> 4bf74025ccc4afd0a61ce8a76d95989fa50e9130
---
+++ rm.sh
@@ -0,0 +1,31 @@
+#!/bin/dash
+
+FIC_NAME=$(basename "$1")
+
+if test ! -f "$1";then
+    echo "Error : file '$FIC_NAME' does not exist"
+    exit 1
+fi
+
+while test "$CHOICE" != "no" && test "$CHOICE" != "yes";do
+echo "Are you sure you want to delete '$FIC_NAME' from versioning? (yes/no)"
+echo -n ">"
+CHOICE=$(cat)
+echo "\n"
+done
+
+if test "$CHOICE" = "no";then
+    echo "Deletion aborted"
+    exit 0
+fi
+
+if test "$CHOICE" = "yes";then
+    for FIC in .version/$FIC_NAME*;do
+        rm "$FIC"
+        rmdir .version 2> /dev/null
+    done
+    echo "Eveyiting was succesfully deleted"
+    exit 0
+fi
+
+exit 0
