a87837e38c4291b5957f45bc412768b70663122d
wernom <loic.sagona@gmail.com>
2018-02-27 18:08:06 +0100

    ajout de versionOK.sh

--- commit.sh
+++ commit.sh
@@ -1,14 +1,19 @@
 #!/bin/dash
 
+
+
+
+
+FIC_NAME=$(basename "$1")
+FIC_PATH=$(dirname "$1")
+
+
 USAGE="Usage: version.sh commit file 'comment'"
 
 if [ ! -f "$1" ];then
 	echo "Error: The file $1 does not exist"
 	exit 1
 fi
-
-FIC_NAME=$(basename "$1")
-FIC_PATH=$(dirname "$1")
 
 if [ ! -d "$FIC_PATH"/.version ] || [ ! -f "$FIC_PATH"/.version/"$FIC_NAME".1 ];then
 	echo "Error: The file '$FIC_NAME' is not under versioning, please use add." >&2
